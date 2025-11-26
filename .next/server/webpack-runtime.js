224403ec893161da477dc3bbaca6887d9f802a04
Ruby <ruby.hoseinzadeh@gmail.com>
2025-11-26 00:04:07 -0800

    modify .gitignore

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("94ca91f5e117b2f3")
+/******/ 		__webpack_require__.h = () => ("0fde598a6cb222dd")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
